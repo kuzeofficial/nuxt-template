6bfb272c1e9cc0ad48809c2679570ca244af4aa0
Cristian Fonseca <cfonsecacomas@gmail.com>
2024-01-13 05:02:18 -0300

    🚀 feat(app.nuxt.test.ts): add test file for Nuxt environment testing
    🚀 feat(nuxt.config.ts): add '@nuxt/test-utils/module' to modules array to enable Nuxt test utilities
    
    🚀 feat(package.json): add '@nuxt/test-utils', 'vitest', 'happy-dom', 'playwright-core', and '@vue/test-utils' as devDependencies for testing purposes
    
    🚀 feat(vitest.config.ts): add configuration for Nuxt test environment with mock options for intersectionObserver and indexedDb

---
+++ app.nuxt.test.ts
@@ -0,0 +1,5 @@
+import { test } from 'vitest'
+
+test('my test', () => {
+  // ... test with Nuxt environment!
+})
---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineVitestConfig } from '@nuxt/test-utils/config'
+
+export default defineVitestConfig({
+    test: {
+        environmentOptions: {
+            nuxt: {
+              mock: {
+                intersectionObserver: true,
+                indexedDb: true,
+              }
+            }
+        }
+    }
+})
